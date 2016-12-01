06ee4394eb84c3d7fdea583bed38778900451c1f
PENVEN YANN p1406611 <yann.penven@etu.univ-lyon1.fr>
2016-12-01 12:29:40 +0100

    add tweet send and layout

--- Tweet In Swift/ViewController.swift
+++ Tweet In Swift/ViewController.swift
@@ -7,11 +7,60 @@
 //
 
 import UIKit
+import Social
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController, UIPickerViewDataSource , UIPickerViewDelegate {
+    
+
+    @IBOutlet weak var tweetTextView: UITextField!
+    
+    @IBAction func sendTweet(_ sender: UIButton) {
+        var action: String
+        var feeling: String
+        var tweetFinal = ""
+        
+        action = self.pickerDataSourceAction[self.pickerView.selectedRow(inComponent: pickerAction)]
+        feeling = self.pickerDataSourceFeeling[self.pickerView.selectedRow(inComponent: pickerFeeling)]
+        if(SLComposeViewController.isAvailable(forServiceType: SLServiceTypeTwitter)) {
+            if let socialController = SLComposeViewController(forServiceType: SLServiceTypeTwitter) {
+                if let sentence = self.tweetTextView.text {
+                    if sentence != "" {
+                        tweetFinal = sentence
+                    }else {
+                        tweetFinal = "je " + action + " " + feeling
+                    }
+                }
+                socialController.setInitialText(tweetFinal)
+                self.present(socialController, animated: true, completion: nil)
+            }
+        }else {
+            let alertController = UIAlertController(title: "Social network", message: "Il n'y à aucun compte tweeter disponible sur le téléphone", preferredStyle: UIAlertControllerStyle.alert)
+            let DestructiveAction = UIAlertAction(title: "Je suis un rebel", style: UIAlertActionStyle.default)
+            let okAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.default)
+            alertController.addAction(DestructiveAction)
+            alertController.addAction(okAction)
+            self.present(alertController, animated: true, completion: nil)
+        }
+    }
+    @IBAction func tapOnBackGround(_ sender: UITapGestureRecognizer) {
+        self.view.endEditing(true)
+    }
+    @IBAction func textViewDidEndOnExit(_ sender: UITextField) {
+        self.view.endEditing(true)
+    }
+    @IBOutlet weak var pickerView: UIPickerView!
+    
+    let pickerDataSourceAction = ["dors","mange", "suis en cours", "galère", "cours", "poireauter"]
+    let pickerDataSourceFeeling = [":)",":(", ";)", ":D", "xD", ":|"]
+    
+    private let pickerAction = 0
+    private let pickerFeeling = 1
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.pickerView.dataSource = self;
+        self.pickerView.delegate = self;
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +69,32 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 2
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        var selectedComponentCount = 0;
+        if component == pickerAction {
+            selectedComponentCount = pickerDataSourceAction.count
+        }else {
+            selectedComponentCount = pickerDataSourceFeeling.count
+        }
+        return selectedComponentCount;
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        if component == pickerAction {
+            return pickerDataSourceAction[row]
+        }else {
+            return pickerDataSourceFeeling[row]
+        }
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int)
+    {
+        
+    }
+    
 }
 
